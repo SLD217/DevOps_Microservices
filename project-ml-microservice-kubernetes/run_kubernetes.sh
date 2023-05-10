ddeac1a57aad3364cd7f4077848141407e713ad6
EC2 Default User <ec2-user@ip-172-31-27-95.eu-west-1.compute.internal>
2023-05-10 12:25:25 +0000

    Completed all tasks as far as task 7
     Committer: EC2 Default User <ec2-user@ip-172-31-27-95.eu-west-1.compute.internal>
    
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   Dockerfile
    	modified:   app.py
    	new file:   kubectl
    	new file:   minikube-linux-amd64
    	modified:   output_txt_files/docker_out.txt
    	modified:   output_txt_files/kubernetes_out.txt
    	modified:   run_docker.sh
    	modified:   run_kubernetes.sh
    	modified:   upload_docker.sh
    
     Changes not staged for commit:
    	modified:   ../Supporting-material/resize.sh
    
     Untracked files:
    	.circleci/

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -6,13 +6,25 @@
 # This is your Docker ID/path
 # dockerpath=<>
 
+dockerpath='mccowna/houseprice:v1.0.0'
+
 # Step 2
 # Run the Docker Hub container with kubernetes
 
+#minikube start
+kubectl get nodes
+kubectl create deploy houseprice --image=$dockerpath 
+
+#--labels app=houseprice
 
 # Step 3:
 # List kubernetes pods
 
+kubectl get deploy,rs,svc,pods
+
 # Step 4:
 # Forward the container port to a host
+#kubectl wait --for=condition=ready pod -l app=houseprice --timeout=180s
+sleep 30
 
+kubectl port-forward deployment/houseprice --address 0.0.0.0 8000:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -7,9 +7,17 @@
 # Create dockerpath
 # dockerpath=<your docker ID/path>
 
+dockerpath='mccowna/houseprice:v1.0.0'
+
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
+docker login --username mccowna --password N0rr1sDR1
+
+docker tag houseprice $dockerpath
+
 # Step 3:
 # Push image to a docker repository
+
+docker push $dockerpath
